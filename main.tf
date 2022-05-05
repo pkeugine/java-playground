c6fd88e9b47a5f406a8d2aa07fbe6d3368acb024
Eugine Park <pkeugine@gmail.com>
2022-05-06 04:23:00 +0900

    feat: try terraform cloud

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
-    organization = "example-organization"
+    organization = "pkeugine"
 
     # The name of the Terraform Cloud workspace to store Terraform state files in.
     workspaces {
-      name = "example-workspace"
+      name = "test"
     }
   }
 }
